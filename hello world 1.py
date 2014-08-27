4443f55ccdb990d50acb3b60a2ae28fb0d2cd15e
michaylabedell <michaylabedell@gmail.com>
2014-08-26 20:45:51 -0400

    first program ever

---
+++ hello world 1.py
@@ -0,0 +1,10 @@
+print('Who is Eren Jegar?')
+answer = input() 
+
+print('you answred : ', answer) 
+
+print('what does 黒執事 mean?')
+answer = input()
+
+if answer.startswith ('Black Butler'):
+    print("Awesome! You read Japanese well!")
