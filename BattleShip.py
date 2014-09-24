44ad3f7c2133ab71c69dc4aa14abad2efb1fcd42
Michayla Bedell <michaylabedell@gmail.com>
2014-09-24 18:24:17 -0400

    More BattleShip

--- BattleShip.py
+++ BattleShip.py
@@ -23,3 +23,27 @@ print(Battleship.size)
 print(Submarine.size)
 print(Destroyer.size)
 print(PatrolBoat.size) 
+
+w = '~'
+hit = '*'
+
+
+grid = [
+    [' ',0,1,2,3,4,5,6,7,8,9],
+    ['A',w,w,w,w,w,w,w,w,w,w],
+    ['B',w,w,w,w,w,w,w,w,w,w],
+    ['C',w,w,w,w,w,w,w,w,w,w],
+    ['D',w,w,w,w,w,w,w,w,w,w],
+    ['E',w,w,w,w,w,w,w,w,w,w],
+    ['F',w,w,w,w,w,w,w,w,w,w],
+    ['G',w,w,w,w,w,w,w,w,w,w],
+    ['H',w,w,w,w,w,w,w,w,w,w],
+    ['I',w,w,w,w,w,w,w,w,w,w],
+    ['J',w,w,w,w,w,w,w,w,w,w]
+]
+
+
+for row in grid:
+    for col in row:
+        print(col,end=' ')
+    print("")
---
+++ Carrier.py
@@ -0,0 +1,14 @@
+class Ship():
+    size = none
+
+class Carrier(Ship):
+    size = 5
+
+
+
+player_carrier = Carrier()
+enemy_carrier = carrier()
+
+
+print(player_carter.size)
+print(enemy_carrier.size)
