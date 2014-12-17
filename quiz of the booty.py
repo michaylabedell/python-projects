bda4de7d904abfbff6a49394f19ddd20ebfba941
Michayla Bedell <michaylabedell@gmail.com>
2014-12-17 17:09:28 -0500

    quiz

---
+++ quiz of the booty.py
@@ -0,0 +1,17 @@
+print("welcome to the booty quiz")
+
+def question():
+    answer =input("what is your name")
+    if answer == "jaja":
+        print("hi")
+        print('correct.')
+    elif answer == 'maybe':
+        print('tell me you nub')
+    else:
+        print('thats cool')
+
+while True: 
+    question()
+        
+
+
